98ce9ca997dc020177ace2bcca48c8c8b3d28bab
neiderm <ded.reckning@gmail.com>
2022-08-14 13:32:27 -0400

    Fix broken .sh script

--- pacman_rel004_sp3e_papilio/scripts/build_roms_pacman.sh
+++ pacman_rel004_sp3e_papilio/scripts/build_roms_pacman.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-# G. Neidermeier 11-Aug-2022 convert .bat to .sh
+#!/bin/sh
+# Converted to .sh by Glenn Neidermeier 6-August-2022
 
 # @echo off
 # 
@@ -25,9 +25,7 @@ romgen_path=../romgen_source
 # REM concatenate consecutive ROM regions
 # copy /b/y %rom_path_src%\pacman.5e + %rom_path_src%\pacman.5f %rom_path%\gfx1.bin > NUL
 # copy /b/y %rom_path_src%\namcopac.6e + %rom_path_src%\namcopac.6f + %rom_path_src%\namcopac.6h + %rom_path_src%\namcopac.6j %rom_path%\main.bin > NUL
-cat $rom_path_src/pacman.5e $rom_path_src/pacman.5f > $rom_path/gfx1.bin
-cat $rom_path_src/namcopac.6e $rom_path_src/namcopac.6f $rom_path_src/namcopac.6h $rom_path_src/namcopac.6j > $rom_path/main.bin
-
+# 
 # REM generate RTL code for small PROMS
 # %romgen_path%\romgen %rom_path_src%\82s126.1m     PROM1_DST  9 l r e     > %rom_path%\prom1_dst.vhd
 # %romgen_path%\romgen %rom_path_src%\82s126.4a     PROM4_DST  8 l r e     > %rom_path%\prom4_dst.vhd
@@ -35,19 +33,32 @@ cat $rom_path_src/namcopac.6e $rom_path_src/namcopac.6f $rom_path_src/namcopac.6
 # REM %romgen_path%\romgen %rom_path_src%\82s126.1m     PROM1_DST  9 c     > %rom_path%\prom1_dst.vhd
 # REM %romgen_path%\romgen %rom_path_src%\82s126.4a     PROM4_DST  8 c     > %rom_path%\prom4_dst.vhd
 # REM %romgen_path%\romgen %rom_path_src%\82s123.7f     PROM7_DST  4 c     > %rom_path%\prom7_dst.vhd
-$romgen_path/romgen $rom_path_src/82s126.1m     PROM1_DST  9 l r e     > $rom_path/prom1_dst.vhd
-$romgen_path/romgen $rom_path_src/82s126.4a     PROM4_DST  8 l r e     > $rom_path/prom4_dst.vhd
-$romgen_path/romgen $rom_path_src/82s123.7f     PROM7_DST  4 l r e     > $rom_path/prom7_dst.vhd
-
+# 
 # REM generate RAMB structures for larger ROMS
 # %romgen_path%\romgen %rom_path%\gfx1.bin          GFX1      13 l r e > %rom_path%\gfx1.vhd
 # %romgen_path%\romgen %rom_path%\main.bin          ROM_PGM_0 14 l r e > %rom_path%\rom0.vhd
+# 
+# REM this is ROM area not used but required
+# %romgen_path%\romgen %rom_path%\gfx1.bin          ROM_PGM_1 13 l r e > %rom_path%\rom1.vhd
+
+# REM concatenate consecutive ROM regions
+cat $rom_path_src/pacman.5e $rom_path_src/pacman.5f > $rom_path/gfx1.bin
+cat $rom_path_src/namcopac.6e $rom_path_src/namcopac.6f $rom_path_src/namcopac.6h $rom_path_src/namcopac.6j > $rom_path/main.bin
+
+# REM generate RTL code for small PROMS
+$romgen_path/romgen $rom_path_src/82s126.1m     PROM1_DST  9 l r e     > $rom_path/prom1_dst.vhd
+$romgen_path/romgen $rom_path_src/82s126.4a     PROM4_DST  8 l r e     > $rom_path/prom4_dst.vhd
+$romgen_path/romgen $rom_path_src/82s123.7f     PROM7_DST  4 l r e     > $rom_path/prom7_dst.vhd
+# REM $romgen_path/romgen $rom_path_src/82s126.1m     PROM1_DST  9 c     > $rom_path/prom1_dst.vhd
+# REM $romgen_path/romgen $rom_path_src/82s126.4a     PROM4_DST  8 c     > $rom_path/prom4_dst.vhd
+# REM $romgen_path/romgen $rom_path_src/82s123.7f     PROM7_DST  4 c     > $rom_path/prom7_dst.vhd
+
+# REM generate RAMB structures for larger ROMS
 $romgen_path/romgen $rom_path/gfx1.bin          GFX1      13 l r e > $rom_path/gfx1.vhd
 $romgen_path/romgen $rom_path/main.bin          ROM_PGM_0 14 l r e > $rom_path/rom0.vhd
 
 # REM this is ROM area not used but required
-# %romgen_path%\romgen %rom_path%\gfx1.bin          ROM_PGM_1 13 l r e > %rom_path%\rom1.vhd
 $romgen_path/romgen $rom_path/gfx1.bin          ROM_PGM_1 13 l r e > $rom_path/rom1.vhd
 
-# echo done
+echo done
 # pause
